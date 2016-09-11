b212af2cc5033d8dc41a4d5ff4ce41f42ded66cb
Lisa Gay <lisagay@alumni.uchicago.edu>
2016-09-11 13:38:57 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lisa Gay's DWA15 Course Portfolio
+
+This page collects my projects for the course Dynamic Web Applications. As I complete projects, I will add links to each project's Github repository and live site.
+
+## Links
+[View the live page](http://p1.josquinia.org/)
+View the screencast demo _LINK TO COME_
+
+## Credits
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title="Lisa Gay P1 - DWA15 Portfolio">
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Lisa Gay</h1>
+    
+    <img src="LisaPicSmall.jpg" alt="Photo of Lisa Gay smiling">
+    
+    <h2>Project 2</h2>
+    <p>Live: <em>link to come</em></p>
+    <p>Github: <em>linke to come</em></p>
+    
+    <h2>Project 3</h2>
+    <p>Live: <em>link to come</em></p>
+    <p>Github: <em>linke to come</em></p>
+    
+    <h2>Project 4</h2>
+    <p>Live: <em>link to come</em></p>
+    <p>Github: <em>linke to come</em></p>
+    
+</body>
+    
+</html>
