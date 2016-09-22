64db6ea15ef241c43633601ea3e91005f07940d0
Lisa Gay <lisagay@alumni.uchicago.edu>
2016-09-22 01:14:01 -0400

    Added P2 links

--- index.php
+++ index.php
@@ -8,21 +8,21 @@
 </head>
 <body>
     <h1>Lisa Gay</h1>
-    
+
     <img id="portrait" src="LisaPicSmall.jpg" alt="Photo of Lisa Gay smiling">
-    
+
     <h2>Project 2</h2>
-    <p>Live: <em>link to come</em></p>
-    <p>Github: <em>link to come</em></p>
-    
+    <p>Live: <em><a href="http://p2.josquinia.org">Jabberwocky Password Generator</a></em></p>
+    <p>Github: <em><a href="https://github.com/ATechWriter/p2">P2 Repository</a></em></p>
+
     <h2>Project 3</h2>
     <p>Live: <em>link to come</em></p>
     <p>Github: <em>link to come</em></p>
-    
+
     <h2>Project 4</h2>
     <p>Live: <em>link to come</em></p>
     <p>Github: <em>link to come</em></p>
-    
+
 </body>
-    
+
 </html>
